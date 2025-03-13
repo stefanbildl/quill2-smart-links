07027ed2962a429e8c25bdde0af83fce3fdddc37
Stefan Bildl <stefanbildl@me.com>
2025-03-13 20:48:08 +0100

    build

--- dist/main.js
+++ dist/main.js
@@ -1,28 +1,29 @@
-const I = /https?:\/\/[^\s]+/;
-class m {
-  constructor(n, f) {
-    n.on("text-change", function(d) {
-      const i = n.getSelection(!1), t = i == null ? void 0 : i.index;
+const m = /https?:\/\/[^\s]+/;
+class y {
+  constructor(n, d) {
+    n.on("text-change", function(i) {
+      var x, u;
+      const f = n.getSelection(!1), t = f == null ? void 0 : f.index;
       if (t === null || typeof t > "u")
         return;
       const [r] = n.getLeaf(t);
       if (r == null)
         return;
-      const [x] = d.ops.filter((s) => s.insert).map((s) => s.insert) ?? [null], o = r.value();
+      const [g] = ((u = (x = i == null ? void 0 : i.ops) == null ? void 0 : x.filter((s) => s.insert)) == null ? void 0 : u.map((s) => s.insert)) ?? [null], o = r.value();
       if (!o || typeof o != "string" || r.parent.domNode.localName === "a")
         return;
-      const u = [`
-`, "	"].find((s) => s === x), g = f.linkRegex ?? I, [e] = o.match(g) ?? [null];
+      const p = [`
+`, "	"].find((s) => s === g), I = d.linkRegex ?? m, [e] = o.match(I) ?? [null];
       if (e === null)
         return;
       const c = o.indexOf(e);
       if (c === null)
         return;
-      const l = n.getIndex(r), a = l + c, p = a + e.length;
-      !u && t <= p && t > a || (n.deleteText(l + c, e.length, "api"), n.insertText(l + c, e, "link", e));
+      const l = n.getIndex(r), a = l + c, k = a + e.length;
+      !p && t <= k && t > a || (n.deleteText(l + c, e.length, "api"), n.insertText(l + c, e, "link", e));
     });
   }
 }
 export {
-  m as SmartLinks
+  y as SmartLinks
 };
